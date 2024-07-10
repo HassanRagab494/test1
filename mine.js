5261bb10e7766964471d7f086a57e2a2a97792be
HassanRagab494 <169075197+HassanRagab494@users.noreply.github.com>
2024-07-10 18:47:59 +0300

    first commit

---
+++ mine.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".landing-loader");
+  document.body.style.overflow = "hidden";
+
+  setTimeout(() => {
+    loader.remove();
+    document.body.style.overflow = "auto";
+  }, 800)
+});
